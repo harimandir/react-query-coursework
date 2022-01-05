f31b2ac6d4c26e36e56277266506af9915a91907
harimandir <harimandir@outlook.com>
2022-01-05 12:13:12 -0700

    Implement delete mutation

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useQuery, useMutation } from "react-query";
 
 async function fetchComments(postId) {
   const response = await fetch(
@@ -26,6 +26,7 @@ async function updatePost(postId) {
 export function PostDetail({ post }) {
   // replace with useQuery
   const { data, isLoading, isError, error } = useQuery(['postComments', post.id], () => fetchComments(post.id));
+  const deleteMutation = useMutation((postId) => deletePost(postId));
   if (isLoading) {
     return <h2>Loading...</h2>
   }
@@ -39,7 +40,16 @@ export function PostDetail({ post }) {
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <button onClick={() => deleteMutation.mutate(post.id)}>Delete</button> <button>Update title</button>
+      { deleteMutation.isLoading && (
+        <p style={{ color: 'gray' }}>Deleting the post</p>
+      )}
+      { deleteMutation.isError && (
+        <p style={{ color: 'red' }}>Error deleting the post</p>
+      )}
+      { deleteMutation.isSuccess && (
+        <p style={{ color: 'green' }}>Deleted the post (but not)</p>
+      )}
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
