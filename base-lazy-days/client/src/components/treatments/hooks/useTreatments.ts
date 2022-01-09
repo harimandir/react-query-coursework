b3a580cae3689fe95fddf35f04c2d7f2acbfbb62
harimandir <harimandir@outlook.com>
2022-01-08 19:38:54 -0700

    Display a toast message for query errors

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -12,7 +12,16 @@ async function getTreatments(): Promise<Treatment[]> {
 }
 
 export function useTreatments(): Treatment[] {
-  const { data = [] } = useQuery(queryKeys.treatments, getTreatments);
+  const toast = useCustomToast();
+  const { data = [] } = useQuery(queryKeys.treatments, getTreatments, {
+    onError: (error) => {
+      const title =
+        error instanceof Error
+          ? error.message
+          : 'error connecting to the server';
+      toast({ title, status: 'error' });
+    },
+  });
 
   return data;
 }
