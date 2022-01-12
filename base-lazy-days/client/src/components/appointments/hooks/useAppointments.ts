50e5d758c6925ba277c3bfea453ff9f9e53878fc
harimandir <harimandir@outlook.com>
2022-01-12 10:20:21 -0700

    Load appointments data for monthly calendars

--- base-lazy-days/client/src/components/appointments/hooks/useAppointments.ts
+++ base-lazy-days/client/src/components/appointments/hooks/useAppointments.ts
@@ -1,6 +1,7 @@
 // @ts-nocheck
 import dayjs from 'dayjs';
 import { Dispatch, SetStateAction, useState } from 'react';
+import { useQuery } from 'react-query';
 
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
@@ -62,15 +63,11 @@ export function useAppointments(): UseAppointments {
   /** ****************** END 2: filter appointments  ******************** */
   /** ****************** START 3: useQuery  ***************************** */
   // useQuery call for appointments for the current monthYear
-
-  // TODO: update with useQuery!
-  // Notes:
-  //    1. appointments is an AppointmentDateMap (object with days of month
-  //       as properties, and arrays of appointments for that day as values)
-  //
-  //    2. The getAppointments query function needs monthYear.year and
-  //       monthYear.month
-  const appointments = {};
+  const { data: appointments = {} } = useQuery(
+    [queryKeys.appointments, monthYear.year, monthYear.month],
+    () => getAppointments(monthYear.year, monthYear.month),
+    {},
+  );
 
   /** ****************** END 3: useQuery  ******************************* */
 
