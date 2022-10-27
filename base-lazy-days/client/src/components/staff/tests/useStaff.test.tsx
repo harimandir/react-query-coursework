fec10f9c65a144126be49ffeeed03b8d2a4fa4f9
harimandir <harimandir@outlook.com>
2022-10-26 23:28:28 -0700

    Add test for filter of useStaff hook

--- base-lazy-days/client/src/components/staff/tests/useStaff.test.tsx
+++ base-lazy-days/client/src/components/staff/tests/useStaff.test.tsx
@@ -1,5 +1,30 @@
+import { act, renderHook } from '@testing-library/react-hooks';
+
+import { Staff } from '../../../../../shared/types';
+import { createQueryClientWrapper } from '../../../test-utils';
 import { useStaff } from '../hooks/useStaff';
 
 test('filter staff', async () => {
-  // the magic happens here
+  const { result, waitFor } = renderHook(useStaff, {
+    wrapper: createQueryClientWrapper(),
+  });
+
+  let allStaff: Staff[];
+
+  await waitFor(() => {
+    const { staff, filter } = result.current;
+    expect(filter).toBe('all');
+    allStaff = Object.values(staff).flat();
+    expect(allStaff.length).toBeGreaterThan(0);
+  });
+
+  const { setFilter } = result.current;
+  act(() => setFilter('massage'));
+
+  await waitFor(() => {
+    const { staff, filter } = result.current;
+    expect(filter).toBe('massage');
+    const massageStaff = Object.values(staff).flat();
+    expect(massageStaff).not.toEqual(allStaff);
+  });
 });
