b9d5dcbc7b0a86a914760b00b2c4996e580a01d5
harimandir <harimandir@outlook.com>
2022-10-26 19:31:29 -0700

    Configure msw to handle api requests for unit tests

--- base-lazy-days/client/src/setupTests.js
+++ base-lazy-days/client/src/setupTests.js
@@ -4,14 +4,18 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 
-// import { server } from './mocks/server.js';
+import { server } from './mocks/server';
 
-// // Establish API mocking before all tests.
-// beforeAll(() => server.listen());
+// Establish API mocking before all tests.
+beforeAll(() =>
+  server.listen({
+    onUnhandledRequest: 'warn',
+  }),
+);
 
-// // Reset any request handlers that we may add during the tests,
-// // so they don't affect other tests.
-// afterEach(() => server.resetHandlers());
+// Reset any request handlers that we may add during the tests,
+// so they don't affect other tests.
+afterEach(() => server.resetHandlers());
 
-// // Clean up after the tests are finished.
-// afterAll(() => server.close());
+// Clean up after the tests are finished.
+afterAll(() => server.close());
