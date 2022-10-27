f51a1871f37d125f99dcc88067ea6616f4e2aca0
harimandir <harimandir@outlook.com>
2022-10-26 20:24:03 -0700

    Test that error alert renders for AllStaff

--- base-lazy-days/client/src/components/staff/tests/AllStaff.test.tsx
+++ base-lazy-days/client/src/components/staff/tests/AllStaff.test.tsx
@@ -1,8 +1,7 @@
 import { screen } from '@testing-library/react';
+import { rest } from 'msw';
 
-// import { rest } from 'msw';
-// import { defaultQueryClientOptions } from '../../../react-query/queryClient';
-// import { server } from '../../../mocks/server';
+import { server } from '../../../mocks/server';
 import { renderWithQueryClient } from '../../../test-utils';
 import { AllStaff } from '../AllStaff';
 
@@ -15,10 +14,13 @@ test('renders response from query', async () => {
 });
 
 test('handles query error', async () => {
-  // (re)set handler to return a 500 error for staff
-  // server.resetHandlers(
-  //   rest.get('http://localhost:3030/staff', (req, res, ctx) => {
-  //     return res(ctx.status(500));
-  //   }),
-  // );
+  server.use(
+    rest.get('http://localhost:3030/staff', (req, res, ctx) => {
+      return res.once(ctx.status(500));
+    }),
+  );
+
+  renderWithQueryClient(<AllStaff />);
+  const alert = await screen.findByRole('alert');
+  expect(alert).toHaveTextContent('Request failed with status code 500');
 });
